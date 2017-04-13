40a6d5de8bfa5a306f7fc21932fb7b729b57bd3f
david di Marcantonio <david@code4mac.com>
2017-04-13 09:40:55 +0200

    add connector

---
+++ Model/Connector.php
@@ -0,0 +1,37 @@
+<?php
+class Pdo
+{
+    /**
+    * function to connect to bdd 
+    * @return PDO
+    */
+    public static function Connect() 
+    {
+        $host = '10.111.61.148';
+        $bdd = 'interventionbdd';
+        $user = 'csharp';
+        $password = 'csharp';
+        try
+        {
+            $mysqlPDO = new PDO("mysql:host=$host;dbname=$bdd;charset=utf8",
+            $user, $password,
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        catch(Exception $e)
+        { // en cas erreur on affiche un message et on arrete tout
+            die('<h1>Erreur de connexion : </h1>' . $e->getMessage());
+        }
+        return $mysqlPDO;
+    }
+
+    /**
+    * function which disconnects bdd 
+    */
+    public static function Disconnect() 
+    {
+        // TODO : disconnect BDD
+
+    }
+}
+
+?>
