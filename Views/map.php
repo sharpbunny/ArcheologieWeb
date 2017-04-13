0a5b6159c1744f1fb66520c8f0d8e44ffe6a5579
FRANCE Frédéric <frederic.france@free.fr>
2017-04-13 16:40:56 +0200

    wip

--- Views/map.php
+++ Views/map.php
@@ -1,5 +1,19 @@
 <?php
+// page title
+$title = "map";
+// page footer contenyt
+$footer = "Contenu bas de page";
+// buffer init
+ob_start();
 
-// file with html and php
+echo '<div id="map"></div>';
+
+// store buffer into $content
+$content = ob_get_clean();
+
+$user = "";
+
+// call template to display
+include('Views/sitetemplate.php');
 
 ?>
