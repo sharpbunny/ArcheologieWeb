45729f9010df2c19c683e613cb134dad8a37fd74
waldodev <hinakoshijou@outlook.fr>
2017-04-13 11:56:37 +0200

    ajout map.php model&controller

---
+++ Model/mapModel.class.php
@@ -0,0 +1,32 @@
+<?php
+//include'pagedeconnectionbdd';
+class Map{
+
+  private $latitude;
+  private $longitude;
+
+  function __contruct($latitude,$longitude)
+  {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+  }
+
+    public function getLatitude()
+    {
+    return $this->latitude;        
+    }
+    public function getLongitude()
+    {
+    return $this->longitude;        
+    }
+
+    public function setLatitude($value)
+    {   
+            $this->latitude=$value;
+    }
+        
+    public function setLongitude($value)
+    {  
+            $this->longitude=$value;
+    }
+}
