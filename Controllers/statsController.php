0aa4c74e8a3407bd798f1c431042fcf2554a08a7
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-04-13 11:21:04 +0200

    Modification syntaxique des conditions dans statsController

--- Controllers/statsController.php
+++ Controllers/statsController.php
@@ -2,10 +2,10 @@
     session_start();
 
     /*On vérifie que l'utilisateur a bien ouvert une session (login et mdp corrects) et qu'il a bien demandé une statistiques*/
-    if(isset($_SESSION['id']) == false){
+    if(!isset($_SESSION['id'])){
 
         /*Si l'utilisateur a demandé à afficher une statistique : listeStats n'est pas vide*/
-        if(empty($_POST['listeStats']) == false){
+        if(!empty($_POST['listeStats'])){
             //On appelle le modèle chargeant les données depuis la bdd
             //On envoie les données à la view pour qu'elle les affiche
             header('Location: ../Views/statsView.php?stats=true'); //A modifier plus tard
