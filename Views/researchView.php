8ec2a238fabd08d0c2efa8f4c0e4367802413e53
Chris.D <fusion101box@gmail.com>
2017-04-13 11:12:44 +0200

    Remise en place d'un PHP simple et compréhensible pour moi avant d'attaque le gros du boulot :thinking:

--- Views/researchView.php
+++ Views/researchView.php
@@ -10,20 +10,23 @@
     <div id="blocRecherche">
         <div id="divBarreRecherche">
             <fieldset>
-                <form action="POST">
+                <form action="researchView.php" method="POST">
                 <label>Champ de recherche</label>
                 <input id="barreRecherche" name="researchField" type="text"></input>
+                <input id="submitResearch" type="submit"></input>
                 </form>
             </fieldset>
-            <?php
-            include("researchSQLAccess.php");
-            ?>
+            
         </div>
         <div id="resultatRecherche">
                 <table>
 
                 </table>
-         
+         <?php
+         //Insertion du Controlleur dans la vue
+         include("../controllers/researchController.php");
+         Research::jeanpierre($_POST['researchField']);
+         ?>
         </div>
     </div>
 </body>
