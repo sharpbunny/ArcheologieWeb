8c7d3c0cc7aed110ea2af7997ada4fce88a620aa
Chris.D <fusion101box@gmail.com>
2017-04-13 08:58:28 +0200

    formatting html and css :thinking:

--- controllers/researchCtrl.php
+++ controllers/researchCtrl.php
@@ -1,4 +1,26 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../stylesheet.css">
+</head>
+
+<body id="bodyRecherche">
+    <div id="blocRecherche">
+        <div id="divBarreRecherche">
+            <fieldset>
+                <label>Champ de recherche</label>
+                <input id="barreRecherche" type="text"></input>
+            </fieldset>
+        </div>
+        <div id="resultatRecherche">
+                <table>
+
+                </table>
+         
+        </div>
+    </div>
+</body>
+
 </html>
