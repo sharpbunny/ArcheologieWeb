352c0f7c1e28e96837738489c06667852ec0b04b
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-04-13 12:05:00 +0200

    Requête sur la BDD fonctionnelle

--- Model/statsModel.php
+++ Model/statsModel.php
@@ -1,24 +1,29 @@
 <?php 
+include ('Connector.php');
     //Si l'utilisateur a bien une session d'ouverte, on autorise sa connexion à la BDD
-    if(isset($_SESSION['id'])){
-        ArcheoPDO::Connect();
+    //if(isset($_SESSION['id'])){
+       $bdd = ArcheoPDO::Connect();
             //On effectue la requête correspondant au choix de l'utilisateur
 
             //Si l'utilisateur a choisi d'afficher les statistiques concernant les thèmes d'intervention'
             if($_POST['listeStats'] == 'themeChart'){
 
+                $theme = $bdd->query('select themeintervention.ID_site, themeintervention.ID_theme, theme.nomTheme FROM themeintervention
+                LEFT JOIN theme ON themeintervention.ID_theme = theme.ID_theme
+                ORDER BY theme.nomTheme ASC');
+                ;
             }
 
             else{
 
-            }
+            //}
         ArcheoPDO::Disconnect();
     }
 
     //Si il n'est pas connecté
-    else{
-        header('Location: ../index.php');
-        exit;      
-    }
+    //else{
+        //header('Location: ../index.php');
+        //exit;      
+    //}
 
 ?>
