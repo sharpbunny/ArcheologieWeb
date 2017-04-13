0021719169cd625918d7d11a4e7a01c815b4cf47
Zerdiron <erdiron@hotmail.fr>
2017-04-13 10:05:31 +0200

    création login

--- Controllers/LoginCtrl.php
+++ Controllers/LoginCtrl.php
@@ -1,6 +1,15 @@
-<?
-
-// First Login Controller 
+<?php 
+// First Login Controller.
+if ( !isset( $_SESSION["id"] ) ) {
+	if ( !isset( $_POST["validerLogin"] ) )
+	{
+		include("./Views/login.php");
+	}
+	else if ( isset( $_POST["validerLogin"] ) and isset( $_POST["pseudoLogin"] ) and isset( $_POST["passwordLogin"] ) )
+	{
+		include("./Model/User.php");
+	}
+}
 
 
 ?>
