c02df72b98854b32a0b79c247306c24b05e7afc3
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-04-13 08:35:58 +0200

    Insertion de la librairie chartjs dans la vue

---
+++ Views/statsView.php
@@ -0,0 +1,17 @@
+<! doctype html>
+
+<html>
+
+    <head>
+    <!-- Vérifier si on doit le laisser dans le head ou le mettre dans le body -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script> <!-- Librairie chartjs-->
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+
+    <body>
+
+
+    </body>
+
+</html>
