96a2d112432fcf1521a804cbaace75a7052abba6
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-04-13 10:47:42 +0200

    Implémentation du formulaire du choix de la statistique

--- Views/statsView.php
+++ Views/statsView.php
@@ -12,13 +12,23 @@
     <body>
         <h1>ArcheologieWeb</h1>
         
+        <!-- Formulaire permettant de choisir la statistique à afficher-->
+        <form action="../Controllers/statsController.php">
+            <select name="listeStats">
+                <option value="themePieChart">Theme Pie Chart </option>
+                <option value="themeBarChart">Theme Bar Chart </option>
+            </select>
+            <input type="Submit" name="chartSubmit" value="Afficher le graphique">
+        </form>
+
+
         <!-- Div contenant le graphique en bâtons -->
         <div id="chartDiv">
             <canvas id="ArcheoChart" width="100" height="100"></canvas>        
         </div>
 
         <div id="pieDiv">
-                    <canvas id="ArcheoPie"></canvas>        
+             <canvas id="ArcheoPie"></canvas>        
         </div>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js" type="text/javascript"></script> <!-- Librairie chartjs-->
