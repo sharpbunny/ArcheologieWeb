39233286feeb85e7e15101cc3ec3a6cf025725ef
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-04-13 09:22:28 +0200

    Graphique en bâtons fonctionnel

--- Views/statsView.js
+++ Views/statsView.js
@@ -1,11 +1,13 @@
-var ctx = document.getElementById("ArcheoChart");
-var myChart = new myChart(ctx, {
-    type :'bar', //graphique en batons
+/* Affichage d'un graphique en bâtons */
+
+var ctx = document.getElementById("ArcheoChart").getContext("2d");
+var myChart = new Chart(ctx, {
+    type :'bar', // bar pour graphique en bâtons
     data : {
-        labels: ["ageJulien", "ageDorian"],
+        labels: ["ageJulien", "ageDorian"], //définition des labels pour chaque barre
         datasets: [{
-            labels:'age des stagiaires de l\'AFPA',
-            data: [26, 29],
+            labels:'age des stagiaires de l\'AFPA', //Nom du graphique
+            data: [26, 29], //Valeurs pour chaque objet
             backgroundColor:[
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)'
@@ -15,16 +17,16 @@ var myChart = new myChart(ctx, {
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)'
             ],
-            borderWidth: 12
+            borderWidth: 1
         }]
     },
     options:{
         scales:{
             yAxes: [{
                 ticks:{
-                    beginAtZero:true
+                    beginAtZero:true //L'axe des Y débute à 0
                 }
             }]
         }
     }
-})
+});
--- Views/statsView.php
+++ Views/statsView.php
@@ -4,16 +4,22 @@
 
     <head>
     <!-- Vérifier si on doit le laisser dans le head ou le mettre dans le body -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script> <!-- Librairie chartjs-->
         <meta charset="utf-8">
+        <link href="statsView.css" rel="stylesheet">
         <title></title>
     </head>
 
     <body>
         <h1>ArcheologieWeb</h1>
-        <canvas id="ArcheoChart" width="400" height="400"></canvas>
+        
+        <!-- Div contenant le graphique en bâtons -->
+        <div id="chartDiv">
+            <canvas id="ArcheoChart" width="100" height="100"></canvas>        
+        </div>
 
-        <script src="statsView.js" type="javascript"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js" type="text/javascript"></script> <!-- Librairie chartjs-->
+        <script src="statsView.js" type="text/javascript"></script>
     </body>
 
 </html>
