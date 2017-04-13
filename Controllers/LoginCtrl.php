0e7fe57b48dc6cdc8445f25e71c46fe2e525cec4
Zerdiron <erdiron@hotmail.fr>
2017-04-13 10:50:50 +0200

    work in progress

--- Controllers/LoginCtrl.php
+++ Controllers/LoginCtrl.php
@@ -2,6 +2,13 @@
 // First Login Controller.
 //FIXME : create class
 
+class Login {
+	static public function DisplayLoginView() {
+		include("./Model/Connector.php");
+		Pdo::Connect();
+	}
+}
+
 if ( !isset( $_SESSION["id"] ) ) {
 	if ( !isset( $_POST["validerLogin"] ) )
 	{
