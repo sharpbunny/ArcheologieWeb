57699ba4ad5072be407f31df3511dd270ed93e1e
Frédéric FRANCE <frederic.france@free.fr>
2017-04-17 17:15:46 +0200

    correct sql

--- Model/mapModel.php
+++ Model/mapModel.php
@@ -17,16 +17,16 @@ class mapModel
             , i.date_debut, i.date_fin
             , c.nomCommune
             , d.nomDepartement
-            , GROUP_CONCAT(p.libellePeriode SEPARATOR '#') as libellePeriodes
-            , GROUP_CONCAT(t.nomTheme SEPARATOR '#') as libelleThemes
+            , GROUP_CONCAT(DISTINCT p.libellePeriode SEPARATOR '#') as libellePeriodes
+            , GROUP_CONCAT(DISTINCT t.nomTheme SEPARATOR '#') as libelleThemes
             FROM site_intervention AS s
-            LEFT JOIN intervention AS i ON i.ID_site = s.ID_site
             LEFT JOIN periodeintervention AS pi ON pi.ID_site = s.ID_site
             LEFT JOIN periode as p ON p.ID_periode = pi.ID_periode
             LEFT JOIN themeintervention AS ti ON ti.ID_site = s.ID_site
             LEFT JOIN theme as t ON t.ID_theme = ti.ID_theme
             LEFT JOIN commune AS c ON c.ID_commune = s.ID_commune
             LEFT JOIN departement AS d ON d.ID_departement = c.ID_departement
+            LEFT JOIN intervention AS i ON i.ID_site = s.ID_site
             GROUP BY s.ID_site
         ");
 
