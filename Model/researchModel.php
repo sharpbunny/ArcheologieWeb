d839ae1e4274fa2fd4bdf247c0e7ff19ff97977c
FRANCE Frédéric <frederic.france@free.fr>
2017-04-19 15:36:07 +0200

    search

--- Model/researchModel.php
+++ Model/researchModel.php
@@ -5,9 +5,8 @@ class SearchModel {
      */
     public static function DisplayResultSearchView() {
         require_once("./Model/Connector.php");
-        if(!isset($_POST['researchField']) ) {
-            $arraySearch = array();
-        } else {
+        $arraySearch = array();
+        if(isset($_POST['researchField']) ) {
             $bdd = ArcheoPDO::Connect();
             $request = $bdd->prepare("SELECT * FROM site_intervention WHERE nom_site LIKE '%".htmlspecialchars($_POST['researchField'])."%'ORDER BY nom_site");
             $request->execute();
